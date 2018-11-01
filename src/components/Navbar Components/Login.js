625bc90844efef93b1a3a49af473a55e73312e3a
Christian Wandtke <wandtke55@gmail.com>
2018-11-01 15:39:18 -0600

    attempted to fix login

--- src/components/Navbar Components/Login.js
+++ src/components/Navbar Components/Login.js
@@ -9,7 +9,7 @@ class Login extends Component{
     login(){
         let {REACT_APP_DOMAIN, REACT_APP_CLIENT_ID} = process.env;
 
-        let uri = `${encodeURIComponent(window.location.origin)}/auth/callback`
+        let uri = encodeURIComponent(`${window.location.origin}/auth/callback`)
 
         window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri${uri}&response_type=code`
     }
