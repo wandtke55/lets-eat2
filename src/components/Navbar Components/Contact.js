1b069b47d0deca22dff4dc0292360c4a4775360e
Christian Wandtke <wandtke55@gmail.com>
2018-11-05 12:04:46 -0700

    more sssstttyyylllleeessss

--- src/components/Navbar Components/Contact.js
+++ src/components/Navbar Components/Contact.js
@@ -6,9 +6,9 @@ export default function Contact(){
             <h2>
                 See a problem with the website? Contact me an let me know what the issue you're seeing is.
             </h2>
-            <h1>
+            <h3>
                 My email is christian.wandtke97@gmail.com
-            </h1>
+            </h3>
             <h4>
                 Have any thoughts about the website? Send me an email, i'd love to hear about any good experiences with the site, and constructive criticism is always appreciated.
             </h4>
