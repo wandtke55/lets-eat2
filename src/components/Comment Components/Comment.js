e0adbe3eed1ff54a2737dc57c3d7a1afd9fd98f4
Christian Wandtke <wandtke55@gmail.com>
2018-11-05 15:01:50 -0700

    just bout done

--- src/components/Comment Components/Comment.js
+++ src/components/Comment Components/Comment.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import DeleteBtn from '../DeleteBtn';
-import StarRating from './StarRating'
 
 class Comment extends Component {
     constructor(props) {
